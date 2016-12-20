4e1503e75f18b2609764c71b61c30e1f86382e84
Paris Kasidiaris <paris@sourcelair.com>
2016-12-21 01:31:46 +0200

    Everything werks

--- static/js/main.jsx
+++ static/js/main.jsx
@@ -4,135 +4,45 @@ import $ from 'jquery';
 import { FlightList } from './components/FlightList.jsx';
 
 
-var flights = [
-  {
-    "from": "ATH",
-    "to": "LHR",
-    "stops": 1
-  },
-  {
-    "from": "ATH",
-    "to": "LHR",
-    "stops": 2
-  },
-  {
-    "from": "ATH",
-    "to": "LHR",
-    "stops": 0
-  },
-  {
-    "from": "LHR",
-    "to": "ATH",
-    "stops": 2
-  },
-  {
-    "from": "LHR",
-    "to": "ATH",
-    "stops": 0
-  },
-  {
-    "from": "LHR",
-    "to": "ATH",
-    "stops": 1
-  },
-  {
-    "from": "ATH",
-    "to": "STN",
-    "stops": 0
-  },
-  {
-    "from": "ATH",
-    "to": "STN",
-    "stops": 1
-  },
-  {
-    "from": "ATH",
-    "to": "STN",
-    "stops": 2
-  },
-  {
-    "from": "STN",
-    "to": "ATH",
-    "stops": 0
-  },
-  {
-    "from": "STN",
-    "to": "ATH",
-    "stops": 1
-  },
-  {
-    "from": "STN",
-    "to": "ATH",
-    "stops": 2
-  },
-  {
-    "from": "ATH",
-    "to": "SFO",
-    "stops": 1
-  },
-  {
-    "from": "ATH",
-    "to": "SFO",
-    "stops": 2
-  },
-  {
-    "from": "SFO",
-    "to": "ATH",
-    "stops": 1
-  },
-  {
-    "from": "SFO",
-    "to": "ATH",
-    "stops": 2
-  },
-  {
-    "from": "JFK",
-    "to": "ATH",
-    "stops": 0
-  },
-  {
-    "from": "JFK",
-    "to": "ATH",
-    "stops": 1
-  },
-  {
-    "from": "JFK",
-    "to": "ATH",
-    "stops": 2
-  },
-  {
-    "from": "ATH",
-    "to": "JFK",
-    "stops": 0
-  },
-  {
-    "from": "ATH",
-    "to": "JFK",
-    "stops": 1
-  },
-  {
-    "from": "ATH",
-    "to": "JFK",
-    "stops": 2
-  },
-  {
-    "from": "ATH",
-    "to": "IKA",
-    "stops": 1
-  },
-  {
-    "from": "ATH",
-    "to": "IKA",
-    "stops": 2
-  },
-  {
-    "from": "IKA",
-    "to": "ATH",
-    "stops": 1
-  }
-];
+var flights = [];
+var flightsContainer = document.getElementById('flights-container');
 
-ReactDOM.render(
-  <FlightList flights={ flights } />,
-  document.getElementById('flights-container')
-);
+function renderFlights(flights) {
+  ReactDOM.render(
+    <FlightList flights={ flights } />,
+    flightsContainer
+  );
+}
+
+$('#filter').click(function () {
+  var fromAirport = $('#from').val();
+  var toAirport = $('#to').val();
+  var stops = parseInt($('#stops').val());
+
+  var filteredFlights = flights.filter(function (flight) {
+    if (fromAirport && (fromAirport != flight.from)) {
+      return false;
+    }
+
+    if (toAirport && (toAirport != flight.to)) {
+      return false;
+    }
+
+    if (stops && (stops != flight.stops)) {
+      return false;
+    }
+
+    return true;
+  });
+
+  renderFlights(filteredFlights);
+});
+
+$('#reset').click(function () {
+  renderFlights(flights);
+})
+
+$.get('/flights/').done(function (data) {
+  flights = data;
+  renderFlights(flights);
+});
