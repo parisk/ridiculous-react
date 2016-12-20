80069c862c743c8388e6e1f878f153478db87a13
Paris Kasidiaris <paris@sourcelair.com>
2016-12-21 00:08:45 +0200

    Flaskify

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 
 export default {
-  entry: 'app/js/main.jsx',
+  entry: 'static/js/main.jsx',
   plugins: [
     babel({
       exclude: 'node_modules/**'
@@ -24,6 +24,6 @@ export default {
       'process.env.NODE_ENV': JSON.stringify('production')
     })
   ],
-  dest: 'app/dist/bundle.js',
+  dest: 'static/dist/bundle.js',
   format: 'iife'
 };
---
+++ server.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', int(os.getenv('PORT', 3000)))
