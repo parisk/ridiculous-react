a4c0bb8f1237b4e71615e5f505cdb55a76101d31
Paris Kasidiaris <paris@sourcelair.com>
2016-12-21 01:59:05 +0200

    Reduce lines

--- static/js/components/FlightList.jsx
+++ static/js/components/FlightList.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Flight } from './Flight.jsx';
 
+
+function FlightStops (props) {
+  if (!props.stops) {
+    return <p>Direct flight</p>;
+  } else if (props.stops == 1) {
+    return <p>1 stop</p>;
+  } else {
+    return <p>{ props.stops } stops</p>;
+  }
+}
+
+function Flight (props) {
+  return (
+    <div className="list-group-item flight">
+      <h4>Flight from { props.from } to { props.to }</h4>
+      <FlightStops stops={ props.stops } />
+    </div>
+  );
+}
 
 export function FlightList (props) {
-  var flightElements = [];
+  var flightElements = props.flights.map(function (flight) {
+    return <Flight from={ flight.from } to={ flight.to } stops={ flight.stops } />
+  });
 
-  for (var i=0; i<props.flights.length; i++) {
-    flightElements.push(
-      <Flight from={ props.flights[i].from } to={ props.flights[i].to } stops={ props.flights[i].stops } />
-    );
-  };
-
-  return (
-    <div class="list-group">
-      { flightElements }
-    </div>
-  )
+  return <div class="list-group">{ flightElements }</div>
 }
