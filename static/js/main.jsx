9eb11508c5687cfe2a40f9982bc80de45b5d32f4
Paris Kasidiaris <paris@sourcelair.com>
2016-12-21 01:50:23 +0200

    jquerize

--- static/js/main.jsx
+++ static/js/main.jsx
@@ -8,10 +8,32 @@ var flights = [];
 var flightsContainer = document.getElementById('flights-container');
 
 function renderFlights(flights) {
-  ReactDOM.render(
-    <FlightList flights={ flights } />,
-    flightsContainer
-  );
+  console.time('render');
+  var innerHTML = '<div class="list-group">';
+  var i;
+  var flight;
+
+  for (i=0; i<flights.length; i++) {
+    flight = flights[i];
+    innerHTML += '<div class="list-group-item flight">';
+    innerHTML += '<h4>Flight from ' + flight.from + ' to ' + flight.to + ' </h4>';
+    innerHTML += '<p>';
+    if (!flight.stops) {
+      innerHTML += 'Direct flight';
+    } else if (flight.stops == 1) {
+      innerHTML += '1 stop';
+    } else {
+      innerHTML += (flight.stops + ' stops');
+    }
+    innerHTML += '</p>';
+    innerHTML += '</div>'
+  }
+
+  innerHTML += '</div>';
+  flightsContainer.innerHTML = innerHTML;
+  console.log(flightsContainer);
+  window.asdasd = innerHTML;
+  console.timeEnd('render');
 }
 
 $('#filter').click(function () {
