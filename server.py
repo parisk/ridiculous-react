ad0c3c7324b4d096767ba0e2a00c3c9f5c171fd8
Paris Kasidiaris <paris@sourcelair.com>
2016-12-21 00:16:47 +0200

    Add flights view

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask import render_template
+from flask import send_file
 
 
 app = Flask(__name__)
@@ -9,6 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/flights/')
+def flights():
+    return send_file('data/flights.json')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', int(os.getenv('PORT', 3000)))
